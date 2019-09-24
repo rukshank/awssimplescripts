91e6ecbe67ca6eb3d8f89dfc4c3a4fd95529b486
Rukshan Kothwala <rukshan.kothwala@pearson.com>
2019-09-24 10:53:34 +0530

    Creating new simple script to find all ec2 instances

---
+++ findInstanceName.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+aws ec2 describe-instances --filters "Name=tag:Name,Values=*" --query 'Reservations[].Instances[].[InstanceId,State.Name,LaunchTime,InstanceType,Tags[?Key==`Name`]| [0].Value]' --output text >> resourcesName.txt
