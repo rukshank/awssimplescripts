a059f8c328bb83b71826661462378b6a89dc0440
Rukshan Kothwala <rukshan.kothwala@pearson.com>
2019-09-24 12:28:27 +0530

    First commit from microsoft code

--- findInstanceName.bash
+++ findInstanceName.bash
@@ -1,2 +1,3 @@
 #!/bin/bash
+#Find All EC2 instances
 aws ec2 describe-instances --filters "Name=tag:Name,Values=*" --query 'Reservations[].Instances[].[InstanceId,State.Name,LaunchTime,InstanceType,Tags[?Key==`Name`]| [0].Value]' --output text >> resourcesName.txt
